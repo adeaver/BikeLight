b1bce36944da55ecc18c7d0206a20910e0befbb7
adeaver <deaver.andrew@gmail.com>
2016-09-09 11:31:26 -0400

    Commented code

--- bikelight_part2/bikelight_part2.ino
+++ bikelight_part2/bikelight_part2.ino
@@ -1,25 +1,33 @@
+// Define the pins, these are all PWM pins
 int rPin1 = 3;
 int yPin1 = 5;
 int gPin = 6;
 int yPin2 = 10;
 int rPin2 = 11;
+
+// Input pins
 int button = 8;
 int dial = A0;
-int mode = 0;
-int lastButtonState = 0;
-int buttonState = 0;
-int countMode = 0;
-int binaryNum = 0;
-float outVoltage = 0;
-unsigned long modeTime = 0;
-unsigned long offTime = -100;
-unsigned long onTime = -100;
-unsigned long lastDebounceTime = 0;
-unsigned long debounceDelay = 50;
-unsigned long blinkDelay = 50;
-unsigned long waveDelay = 100;
-unsigned long binaryDelay = 300;
 
+// State variables
+int mode = 0; // current mode of bike light
+int lastButtonState = 0; // previous button state
+int buttonState = 0; // current button state
+int countMode = 0; // number used to assign modes in wave and binary
+int binaryNum = 0; // current binary number
+float outVoltage = 0; // brightness
+
+// Time Variables 
+unsigned long modeTime = 0; // time in a given mode during wave and binary
+unsigned long offTime = -100; // time LEDs are off during blink
+unsigned long onTime = -100; // time LEDs are on during blink
+unsigned long lastDebounceTime = 0; // time since button last changed
+
+// Delays
+unsigned long debounceDelay = 50; // delay between button presses
+unsigned long blinkDelay = 50; // delay between blinks
+unsigned long waveDelay = 100; // delay between LEDs turning on during wave
+unsigned long binaryDelay = 300; // delay between increments during binary
 
 void setup() {
   pinMode(rPin1, OUTPUT);
@@ -27,11 +35,75 @@ void setup() {
   pinMode(gPin, OUTPUT);
   pinMode(yPin2, OUTPUT);
   pinMode(rPin2, OUTPUT);
+  
   pinMode(button, INPUT);
   pinMode(dial, INPUT);
+  
   Serial.begin(9600);
 }
 
+void loop() {
+  int reading = digitalRead(button);
+  outVoltage = analogRead(dial)/1023.0 * 255;
+  
+  if(reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  
+  if((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if(buttonState) {
+        // If the button is pressed, change modes
+        resetMode();
+      }
+    }
+  }
+  
+  // run the current mode
+  runMode();
+  
+  lastButtonState = reading;
+}
+
+// Increment the mode and start in default states
+void resetMode() {
+  // increment mode between 0-4
+  mode = (mode+1)%5;
+  
+  // start in default mode for wave and binary
+  modeTime = 0;
+  countMode = 0;
+  
+  // if the mode is blink, reset the values for on and off time
+  if(mode == 2) {
+   offTime = -100;
+   onTime = -100; 
+  }  
+}
+
+// runs the appropriate function given the mode
+void runMode() {
+  switch(mode){
+      case 1:
+        turnAllOn();
+        break;
+      case 2:
+        blinkAll();
+        break;
+      case 3:
+        wave();
+        break;
+      case 4:
+        binaryCount();
+        break;
+      default:
+        turnAllOff();
+        break;
+   }  
+}
+
+// This method turns all of the LEDs on
 void turnAllOn() {
   analogWrite(rPin1, outVoltage);
   analogWrite(yPin1, outVoltage);
@@ -40,6 +112,7 @@ void turnAllOn() {
   analogWrite(rPin2, outVoltage); 
 }
 
+// This method turns all of the LEDs off
 void turnAllOff() {
   analogWrite(rPin1, LOW);
   analogWrite(yPin1, LOW);
@@ -48,30 +121,59 @@ void turnAllOff() {
   analogWrite(rPin2, LOW); 
 }
 
+// This method blinks all of the lights
 void blinkAll(){
   // Default State
   if (offTime == -100 && onTime == -100){
-    offTime = millis();
-    turnAllOff();
+    // if both counters are off
+    
+    offTime = millis(); // start counting the time spent in off state
+    turnAllOff(); 
   }
   // Off State has Ended (Begin On State)
-  if (offTime != -100 && millis() - offTime > blinkDelay){
-    onTime = millis();
-    turnAllOn();
-    offTime = -100;
+  if (offTime != -100 && millis() - offTime > blinkDelay) { 
+    // if offTime counter has started and expired
+    
+    onTime = millis(); // begin onTime counter
+    turnAllOn(); 
+    offTime = -100; // turn off off time counter
   }
   // On State has Ended (Begin default)
   if (onTime != -100 && millis() - onTime > blinkDelay){
+    // if the onTime counter has started and expired
+    
     turnAllOff();
+    
+    // turn off both counters
     offTime = -100; 
     onTime = -100;
   }
+  
+  /* Try this:
+  
+  if(millis() - modeTime > blinkDelay) {
+     modeTime = millis();
+     
+     if(modeCount == 0) {
+        turnAllOff(); 
+     } else {
+       turnAllOn();
+     }
+     
+     modeCount = (modeCount+1)%2;
+  }  
+  */
 }
 
+// This method blinks all the lights sequentially
 void wave() {
   if(millis() - modeTime > waveDelay) {
-    modeTime = millis();
-    countMode = (countMode+1)%8;
+    // if the timer between lights has expired
+    
+    modeTime = millis(); // set the timer
+    
+    countMode = (countMode+1)%8; // change the mode
+    
     switch(countMode) {
      case 0:
         waveOn(rPin1);
@@ -93,11 +195,13 @@ void wave() {
 
 }
 
+// This methods turns the correct pin on for the wave
 void waveOn(int pinNumber) {
   turnAllOff();
   analogWrite(pinNumber, outVoltage); 
 }
 
+// This method counts to 31 in binary using the LEDs
 void binaryCount() {
   if(millis() - modeTime > binaryDelay) {
     modeTime = millis();
@@ -106,70 +210,32 @@ void binaryCount() {
   }
 }
 
+// This method takes in a number and determines which pins to light up
 void binaryOn(int num) {
    turnAllOff();
    if(num >= 16) {
+     // lights up the 5th bit
      analogWrite(rPin1, outVoltage);
      num -= 16;
    }
    if(num >= 8) {
+     // lights up the 4th bit
       analogWrite(yPin1, outVoltage);
       num -= 8; 
    }
    if(num >= 4) {
+      // lights up the 3rd bit
       analogWrite(gPin, outVoltage);
       num -= 4; 
    }
    if(num >= 2) {
+      // lights up the 2nd bit
       analogWrite(yPin2, outVoltage);
       num -= 2;
    }
    if(num >= 1) {
+      // lights up the 1st bit
       analogWrite(rPin2, outVoltage);
       num -= 1; 
    }
-}
-
-void loop() {
-  int reading = digitalRead(button);
-  outVoltage = analogRead(dial)/1023.0 * 255;
-  
-  if(reading != lastButtonState) {
-    lastDebounceTime = millis();
-  }
-  
-  if((millis() - lastDebounceTime) > debounceDelay) {
-    if (reading != buttonState) {
-      buttonState = reading;
-      if(buttonState) {
-        mode = (mode+1)%5;
-        modeTime = 0;
-        countMode = 0;
-        if(mode == 2) {
-         offTime = -100;
-         onTime = -100; 
-        }
-      }
-    }
-  }
-  
-  switch(mode){
-      case 1:
-        turnAllOn();
-        break;
-      case 2:
-        blinkAll();
-        break;
-      case 3:
-        wave();
-        break;
-      case 4:
-        binaryCount();
-        break;
-      default:
-        turnAllOff();
-        break;
-   }
-  
-  lastButtonState = reading;
 }
